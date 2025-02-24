567e7073613919a5b6e4c012476f9c9a6edf3750
itzSerag <serag.eldien.mahmoud@gmail.com>
2025-02-25 01:07:35 +0200

    update vite config to suit our backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-  
+
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://vercel.com/serag-eldeins-projects/chatty-serageldien-project', // Use the actual deployed backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
   plugins: [
     react(),
 
