7e87e201db4563159ce210f8e338287ae24dc94d
itzSerag <serag.eldien.mahmoud@gmail.com>
2025-02-25 01:40:28 +0200

    update vite from dist to public

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-
+  build: {
+    outDir: "public"  // Change from "dist" to "public"
+  },
   server: {
     proxy: {
       '/api/v1': {
