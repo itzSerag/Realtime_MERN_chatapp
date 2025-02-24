ae698676a65ee3b5a412a771ba29f6d5755ad234
itzSerag <serag.eldien.mahmoud@gmail.com>
2025-02-25 01:44:58 +0200

    return no dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-  build: {
-    outDir: "public"  // Change from "dist" to "public"
-  },
   server: {
     proxy: {
       '/api/v1': {
