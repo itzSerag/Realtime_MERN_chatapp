0c9b383be7fb2c25f6f5ee8c19795c7e8f5a16d5
itzSerag <serag.eldien.mahmoud@gmail.com>
2025-02-25 02:11:32 +0200

    update vite backend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://chatty-serageldien-project.vercel.app'],
         credentials: true,
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
         preflightContinue: false,
